e2bbc57d15bcdae481445404ecfd4172507a617c
lindsfitz <lindsaytfitz@gmail.com>
2021-10-14 12:01:28 -0700

    beginning code structure written

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+
+// TODO: Create a function to generate markdown for README
+
 const fs = require('fs');
 const fsPromises = require('fs').promises;
 const inquirer = require('inquirer');
@@ -6,60 +17,91 @@ inquirer
     .prompt([
         {
             type: 'input',
-            message: 'Please enter your name.',
-            name: 'username',
+            message: 'Please enter the title of your repo.',
+            name: 'title',
         },
         {
             type: 'input',
-            message: 'Where are you located?',
-            name: 'location',
+            message: 'Please enter the description of this application.',
+            name: 'description',
         },
         {
             type: 'input',
-            message: 'Enter a bio.',
-            name: 'bio',
+            message: 'Enter installation instructions.',
+            name: 'install',
         },
         {
             type: 'input',
-            message: 'Please enter your Linkedin URL.',
-            name: 'linkedin',
+            message: 'Enter usage instructions.',
+            name: 'usage',
+        },
+        {
+            type: 'list',
+            message: 'Choose the license for this application',
+            choices: ['MIT'],
+            name: 'license',
         },
         {
             type: 'input',
-            message: 'Please enter your Github URL.',
+            message: 'Please enter any contribution data.',
+            name: 'contributors',
+        },
+        {
+            type: 'input',
+            message: 'Tests?',
+            name: 'tests',
+        },
+        {
+            type: 'input',
+            message: 'For any potential questions, enter your email address.',
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: 'Enter your Github username.',
             name: 'github',
         },
     ])
     .then((response) =>
-        fsPromises.writeFile(`${response.username}.html`,
-        `<!DOCTYPE html>
-        <html lang="en">
+        fsPromises.writeFile(`README.md`,
+        `# ${response.title}
+
+        ## Description
+            ${response.description}
+
+        ![badge](${licenselink})
+
+        ## Table of Contents 
+            * [Installation](#installation)
+            * [Usage](#usage)
+            * [Questions](#questions)
+            * [License](#license)
         
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Portfolio</title>
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css">
-            <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-        </head>
+        ## Installation
+
+            **Follow these steps to properly install this application:**
+
+            ${response.install}
+
+        ## Usage
+            **How to use this application:**
+            ${response.usage}
+
+        ## License
+            
+
+        ## Contributors
+            ${response.contributors}
+
+        ## Questions
+            **For any additional questions or feedback about this application**
+            Feel free to reach out via email or contact me on Github:
+
+            Email:
+            [${response.email}](mailto:${response.email})
+
+            Github:
+            [${response.github}](https://github.com/${response.github})
         
-        <body>
-            <div class="jumbotron jumbotron-fluid">
-                <div class="container">
-                    <h1 class="display-4" id='username'>${response.username}</h1>
-                    <p class="lead" id='bio'>${response.bio}</p>
-                    <hr class="my-4">
-                    <p id='location'>${response.location}</p>
-                </div>
-        
-                <ul class="list-group">
-                    <a href="${response.linkedin}" class="list-group-item list-group-item-action">LinkedIn</a>
-                    <a href="${response.github}" class="list-group-item list-group-item-action">Github</a>
-                  </ul>
-            </div>
-        
-        </body>
-        
-        </html>`, error => error ? console.log(error) : console.log('Your portfolio has been created!'))
+        `, error => error ? console.log(error) : console.log('Your README has been created!'))
     );
